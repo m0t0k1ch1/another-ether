a677be38cda30669800583349372f1a5038dc066
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-04-29 20:55:46 +0900

    follow zeppelin-solidity 1.9.0

--- contracts/AnotherEther.sol
+++ contracts/AnotherEther.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
 import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
@@ -7,7 +7,7 @@ contract AnotherEther is StandardToken {
   string public symbol = "AETH";
   uint public decimals = 18;
 
-  function AnotherEther(uint initialSupply) public {
+  constructor(uint initialSupply) public {
     totalSupply_ = initialSupply;
     balances[msg.sender] = initialSupply;
   }
