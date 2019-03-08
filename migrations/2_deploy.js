114c4e91adf9b619569c864bcc40374ff32b1b82
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2019-03-09 03:50:13 +0900

    fix migration scripts

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,4 +1,4 @@
-const AnotherEther = artifacts.require('./AnotherEther.sol');
+const AnotherEther = artifacts.require('AnotherEther');
 
 module.exports = async (deployer) => {
   await deployer.deploy(AnotherEther, web3.utils.toBN('21000000e18'), {
