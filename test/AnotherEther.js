af28e6b3a1c17302d953100d77794b6a97933018
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-04-15 21:39:18 +0900

    add tests

--- test/AnotherEther.js
+++ test/AnotherEther.js
@@ -3,10 +3,14 @@ const { expect } = require("chai");
 describe("AnotherEther contract", () => {
   let AETH;
 
+  let aeth;
+
+  let deployer;
+
   beforeEach(async () => {
     AETH = await ethers.getContractFactory("AnotherEther");
-
     aeth = await AETH.deploy();
+    [deployer] = await ethers.getSigners();
   });
 
   describe("deployment", () => {
@@ -14,6 +18,8 @@ describe("AnotherEther contract", () => {
       expect(await aeth.name()).to.equal("AnotherEther");
       expect(await aeth.symbol()).to.equal("AETH");
       expect(await aeth.totalSupply()).to.equal(21000000);
+      expect(await aeth.decimals()).to.equal(18);
+      expect(await aeth.balanceOf(deployer.address)).to.equal(21000000);
     });
   });
 });
