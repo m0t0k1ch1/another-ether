5e4c4130b3e2f889f2c2bb380b4dc68b34521162
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2021-04-15 20:55:43 +0900

    add a deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const AETH = await ethers.getContractFactory("AnotherEther");
+  const aeth = await AETH.deploy();
+  console.log("AETH address:", aeth.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
